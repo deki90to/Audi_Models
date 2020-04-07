188ab504de8c4e9bdd243562340bb1270c131f93
deki90to <deki90to@gmail.com>
2020-04-07 12:11:01 +0200

    added functions to buttons

--- django_app/urls.py
+++ django_app/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('', views.a1, name='a1'),
+	path('2', views.a2, name='a2'),
+	path('3', views.a3, name='a3'),
+	path('4', views.a4, name='a4'),
+	path('5', views.a5, name='a5'),
+	path('6', views.a6, name='a6'),
+	path('7', views.a7, name='a7'),
+	path('8', views.a8, name='a8'),
 ]
--- django_app/views.py
+++ django_app/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
 
 
-def index(request):
-	return render(request, 'index.html')
+
+def a1(request):
+	return render(request, 'a1.html')
+
+def a2(request):
+	return render(request, 'a2.html')
+
+def a3(request):
+	return render(request, 'a3.html')
+
+def a4(request):
+	return render(request, 'a4.html')
+
+def a5(request):
+	return render(request, 'a5.html')
+
+def a6(request):
+	return render(request, 'a6.html')
+
+def a7(request):
+	return render(request, 'a7.html')
+
+def a8(request):
+	return render(request, 'a8.html')
